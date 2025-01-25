1de6951123fb8416f5f6c15162bbf4c3fc838b89
Hardikgarg987 <hardikgarg987@gmail.com>
2025-01-25 10:46:44 +0530

    connected login page to database

--- api/index.js
+++ api/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('./models/User.js');
 require('dotenv').config()
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'aisfohdiofjsidhf309u4noj390'
 
 app.use(express.json());
 app.use(cors({
@@ -19,7 +21,6 @@ mongoose.connect(process.env.MONGO_URL);
 app.get('/test', (req,res) => {
     res.json('test ok');
 });
-//MOBDFUxO6Pl41WA2
 app.post('/register',async (req,res) => {
     const {name,email,password} = req.body;
 
@@ -36,6 +37,24 @@ app.post('/register',async (req,res) => {
         res.status(422).json(e)
     }
 
-})
+});
+
+app.post('/login', async (req,res) => {
+    const{email,password} = req.body;
+    const userDoc = await User.findOne({email});
+    if(userDoc){
+        const passOk = bcrypt.compareSync(password, userDoc.password)
+        if(passOk){
+            jwt.sign({email:userDoc.email, id:userDoc._id}, jwtSecret, {}, (err, token) => {
+                if(err) throw err;
+                res.cookie('token', token).json('pass Ok');
+            })
+        }else{
+            res.status(422).json('pass not ok');
+        }
+    }else{
+        res.json('not found');
+    }
+});
 
 app.listen(4000);
