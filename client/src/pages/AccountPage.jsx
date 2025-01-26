5522c303ae66d54cf8511359a960ff42afbc435e
Hardikgarg987 <hardikgarg987@gmail.com>
2025-01-26 11:39:33 +0530

    designed a page to add a place for a user

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -4,12 +4,13 @@ import { Navigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
+import PlacesPage from "./PlacesPage.jsx";
 export default function AccountPage() {
-    const {ready, user, setUser} = useContext(UserContext);
+    const { ready, user, setUser } = useContext(UserContext);
     const [redirect, setRedirect] = useState(false);
 
-    let {subpage} = useParams();
-    if(subpage == undefined) {
+    let { subpage } = useParams();
+    if (subpage == undefined) {
         subpage = 'profile';
     }
 
@@ -19,39 +20,62 @@ export default function AccountPage() {
         setUser(null);
     }
 
-    if(!ready){
+    if (!ready) {
         return 'Loading...';
     }
 
-    if(ready && !user && !redirect) {
-        return <Navigate to = {'/login'} />
+    if (ready && !user && !redirect) {
+        return <Navigate to={'/login'} />
     }
 
 
 
-    function linkClasses (type = null) {
-        let classes = 'py-2 px-6'
-        if(type == subpage){
-            classes += ' bg-primary text-white rounded-full';
+    function linkClasses(type = null) {
+        let classes = 'inline-flex gap-1 py-2 px-6 rounded-full'
+        if (type == subpage) {
+            classes += ' bg-primary text-white';
+        } else {
+            classes += 'bg-gray-200';
         }
         return classes;
     }
 
-    if(redirect) {
+    if (redirect) {
         return <Navigate to={redirect} />
     }
-    return(
+    return (
         <div>
-            <nav className="w-full flex justify-center mt-8 gap-2 mb-8"> 
-                <Link className={linkClasses('profile')} to={'/account'}> My profile </Link>
-                <Link className={linkClasses('bookings')} to={'/account/bookings'}> My bookings </Link>
-                <Link className={linkClasses('places')} to={'/account/places'}> My accomodations</Link>
+            <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
+                <Link className={linkClasses('profile')} to={'/account'}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+
+                    My profile
+                </Link>
+                <Link className={linkClasses('bookings')} to={'/account/bookings'}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 0 1 0 3.75H5.625a1.875 1.875 0 0 1 0-3.75Z" />
+                    </svg>
+
+                    My bookings
+                </Link>
+                <Link className={linkClasses('places')} to={'/account/places'}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819" />
+                    </svg>
+
+                    My accomodations
+                </Link>
             </nav>
             {subpage === 'profile' && (
                 <div className="text-center max-w-lg mx-auto">
                     Logged in as {user.name} ({user.email}) <br />
                     <button onClick={logout} className="primary max-w-sm mt-2">logout</button>
                 </div>
+            )}
+            {subpage == 'places' && (
+                <PlacesPage />
             )}
         </div>
     );
